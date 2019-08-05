f1a3be3e8654dfc7912c966831361b531304a3b9
songpang <scc6920@naver.com>
2019-08-05 11:04:11 -0700

    configurable chart update

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,30 +1,49 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { Chart } from "chart.js";
 import { DomSanitizer } from "@angular/platform-browser";
+// import {NavController} from 'ionic-angular';
+// import { Tab3Page } from '../tab3/tab3.page';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  selector: "app-tab2",
+  templateUrl: "tab2.page.html",
+  styleUrls: ["tab2.page.scss"]
 })
 export class Tab2Page {
   @ViewChild("lineCanvas") lineCanvas: ElementRef;
+
   private lineChart: Chart;
+  private activeChart: Chart;
+
   test = 1;
   videoId = "https://www.youtube.com/embed/TlQ8txalLYg";
 
-  constructor(private dom: DomSanitizer) {
+  constructor(
+    // public navCtrl:NavController,
+    private dom: DomSanitizer
+  ) {
     // setInterval(() => {
     //   this.test++;
     // }, 1000)
   }
 
-  ngOnInit() {
+  // movePage() {
+  //   this.navCtrl.push(Tab3Page);
+  // }
 
-    this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+  charDataset_weight = {
+    fisrt: {
       type: "line",
       data: {
-        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
         datasets: [
           {
             label: "Kyle's weight change",
@@ -49,15 +68,88 @@ export class Tab2Page {
             spanGaps: false
           }
         ]
+      },
+      options: {
+        title: {
+          text: "hello",
+          display: true
+        }
       }
-    });
+    },
+    second: {
+      type: "line",
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "Kyle's muscle change",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 5,
+            pointHitRadius: 10,
+            data: [35, 35, 32, 32, 33, 35, 36],
+            spanGaps: false
+          }
+        ]
+      }
+    }
+  };
+
+  ngOnInit() {
+    this.activeChart = new Chart(
+      this.lineCanvas.nativeElement,
+      this.charDataset_weight.fisrt
+    );
+    this.lineChart = this.activeChart;
   }
 
   sanitize(videoUrl) {
     return this.dom.bypassSecurityTrustResourceUrl(videoUrl);
   }
-  
+
   dataCompare() {
-    console.log('hello im' + this.lineChart.data.datasets);
+    console.log("hello im : " + this.lineChart.data.datasets.label);
+  }
+
+  changeData() {
+    this.activeChart = new Chart(
+      this.lineCanvas.nativeElement,
+      this.charDataset_weight.second
+    );
+    this.lineChart = this.activeChart;
+    console.log("Hello i'm here" + this.activeChart);
+  }
+
+  updateConfigByMutating() {
+    // this.lineChart.options.title.text = "new title";
+    // this.lineChart.update();
+    // alert("hello");
+  this.lineChart.data.datasets[0].data[2] = 30;
+
+  }
+
+  updataConfigAboutMonth(monthValue) {
+
   }
 }
