f60da4444acfca78edeb314408d3bf7f18c71e36
songpang <scc6920@naver.com>
2019-08-06 15:20:05 -0700

    return fn in chart

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -12,27 +12,22 @@ export class Tab2Page {
 
   private lineChart: Chart;
   private activeChart: Chart;
+  private activeChart2: Chart;
+  private chartcount: number;
   private measureChange: boolean;
+  private periodofMonth: number;
 
   test = 1;
   videoId = "https://www.youtube.com/embed/TlQ8txalLYg";
 
-  constructor(
-    // public navCtrl:NavController,
-    private dom: DomSanitizer
-  ) {
+  constructor(private dom: DomSanitizer) {
     this.measureChange = true;
-    // setInterval(() => {
-    //   this.test++;
-    // }, 1000)
+    this.periodofMonth = 6;
+    this.chartcount = 0;
   }
 
-  // movePage() {
-  //   this.navCtrl.push(Tab3Page);
-  // }
-
   charDataset_weight = {
-    fisrt: {
+    first: {
       type: "line",
       data: {
         labels: [
@@ -58,13 +53,13 @@ export class Tab2Page {
             pointBorderColor: "rgba(75,192,192,1)",
             pointBackgroundColor: "#fff",
             pointBorderWidth: 1,
-            pointHoverRadius: 5,
+            pointHoverRadius: 3,
             pointHoverBackgroundColor: "rgba(75,192,192,1)",
             pointHoverBorderColor: "rgba(220,220,220,1)",
             pointHoverBorderWidth: 2,
-            pointRadius: 1,
+            pointRadius: 3,
             pointHitRadius: 10,
-            data: [85, 81, 80, 81, 79, 73, 71, 81],
+            data: [85, 81, 80, 81, 79, 73, 71, 70, 71, 70, 69, 70],
             spanGaps: false
           },
           {
@@ -84,12 +79,25 @@ export class Tab2Page {
             pointHoverBackgroundColor: "rgba(115, 18, 18,1)",
             pointHoverBorderColor: "rgba(220,220,220,1)",
             pointHoverBorderWidth: 2,
-            pointRadius: 5,
+            pointRadius: 3,
             pointHitRadius: 10,
             data: [90, 88, 89, 91, 91, 90, 92],
             spanGaps: false
           }
         ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                callback: value => {
+                  return value + "kg";
+                }
+              }
+            }
+          ]
+        }
       }
     },
     second: {
@@ -133,69 +141,109 @@ export class Tab2Page {
   };
 
   ngOnInit() {
-    this.activeChart = new Chart(
+    this.lineChart = new Chart(
       this.lineCanvas.nativeElement,
-      this.charDataset_weight.fisrt
+      this.charDataset_weight.first
     );
-    this.lineChart = this.activeChart;
   }
 
   sanitize(videoUrl) {
     return this.dom.bypassSecurityTrustResourceUrl(videoUrl);
   }
 
-  dataCompare() {
-    console.log("hello im : " + this.lineChart.data.datasets.label);
-  }
-
   changeData() {
-    this.activeChart = new Chart(
-      this.lineCanvas.nativeElement,
-      this.charDataset_weight.second
-    );
-    this.lineChart = this.activeChart;
-    console.log("Hello i'm here" + this.activeChart);
-  }
-
-  updateConfigByMutating() {
-    // this.lineChart.options.title.text = "new title";
-    // this.lineChart.data.datasets[0].data = [90, 100, 110, 120, 50, 140, 150, 160];
-
-    this.lineChart.update();
-    alert("hello");
-  }
-
-  updataConfigAboutMonth(month) {
-    
-    let past3month = ["July","August","September","October", "November", "December/18"];
-    for(let i=5;i>=0;i--) {
-      this.lineChart.data.labels.unshift(past3month[i]);
+    if (this.chartcount == 0) {
+      this.lineChart = new Chart(
+        this.lineCanvas.nativeElement,
+        this.charDataset_weight.second
+      );
+      this.lineChart.update();
+      this.chartcount++;
+    } else {
+      this.lineChart = new Chart(
+        this.lineCanvas.nativeElement,
+        this.charDataset_weight.first
+      );
+      this.lineChart.update();
+      this.chartcount--;
     }
-    this.lineChart.data.datasets.forEach(dataset => {
-      dataset.data.unshift(80);
-      dataset.data.unshift(83);
-      dataset.data.unshift(81);
-      dataset.data.unshift(89);
-      dataset.data.unshift(87);
-      dataset.data.unshift(86);
-      
-    });
-    this.lineChart.update();
+  }
 
+  viewpastData() {
+    let past3month = [
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December/18"
+    ];
+
+    let weightData = [80, 84, 82, 83, 90, 92];
+    let __lineChartData = this.lineChart;
+
+    switch (this.periodofMonth) {
+      case 6:
+        this.unshiftData(3, __lineChartData.data, past3month, weightData);
+        __lineChartData.update();
+        this.periodofMonth = this.periodofMonth + 3;
+        break;
+      case 9:
+        this.unshiftData(3, __lineChartData.data, past3month, weightData);
+        __lineChartData.update();
+        this.periodofMonth = this.periodofMonth + 3;
+        break;
+      case 12:
+        __lineChartData.data.labels.splice(0, 6);
+        __lineChartData.data.datasets[0].data.splice(0, 6);
+        __lineChartData.data.datasets[1].data.splice(0, 6);
+        this.periodofMonth = this.periodofMonth - 6;
+        __lineChartData.update();
+        console.log(__lineChartData.data.datasets[0].data);
+        break;
+    }
+  }
+
+  unshiftData(count: number, chartData, dataList, targetData) {
+    for (let i = count - 1; i >= 0; i--) {
+      chartData.labels.unshift(dataList[i]);
+    }
+
+    chartData.datasets.forEach(dataset => {
+      // console.log(dataset.data)
+      for (let i = count - 1; i >= 0; i--) {
+        dataset.data.unshift(targetData[i]);
+      }
+    });
   }
 
   // conversion measure of data
   dataConverse() {
     let userData_weight = this.lineChart.data.datasets[0].data;
+    let userData_weight2 = this.lineChart.data.datasets[1].data;
 
     if (this.measureChange) {
       userData_weight.forEach((item, index, array) => {
         array[index] = item * 2.2;
         array[index] = array[index].toFixed(2);
-        // function of javascript calculating error fix
+        // function of javascript calculating error fix (toFixed fn)
+      });
+      userData_weight2.forEach((item, index, array) => {
+        array[index] = item * 2.2;
+        array[index] = array[index].toFixed(2);
+        // function of javascript calculating error fix (toFixed fn)
       });
 
-      this.lineChart.data.datasets[0].label = "Kyle's weight change ( LB )";
+      this.lineChart.options.scales.yAxes = [
+        {
+          ticks: {
+            callback: (value) => {
+              return value + ' lb';
+            }
+          }
+        }
+      ];
+
       this.lineChart.update();
       this.measureChange = false;
     } else {
@@ -204,11 +252,30 @@ export class Tab2Page {
         array[index] = array[index].toFixed(2);
         // function of javascript calculating error fix
       });
+      userData_weight2.forEach((item, index, array) => {
+        array[index] = item / 2.2;
+        array[index] = array[index].toFixed(2);
+        // function of javascript calculating error fix
+      });
 
-      this.lineChart.data.datasets[0].label = "Kyle's weight change ( KG )";
+      this.lineChart.options.scales.yAxes = [
+        {
+          ticks: {
+            callback: (value) => {
+              return value + ' kg';
+            }
+          }
+        }
+      ];
+
       this.lineChart.update();
       this.measureChange = true;
     }
     console.log(userData_weight);
+  }
+
+  viewfutureData() {
+    this.lineChart.data.labels.push("progress");
+    this.lineChart.update();
   }
 }
