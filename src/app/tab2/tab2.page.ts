25264307b317ddc26ebe79111c95dc8ea6030fae
songpang <scc6920@naver.com>
2019-08-06 09:32:35 -0700

    new line add ( in chart)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -12,6 +12,7 @@ export class Tab2Page {
 
   private lineChart: Chart;
   private activeChart: Chart;
+  private measureChange: boolean;
 
   test = 1;
   videoId = "https://www.youtube.com/embed/TlQ8txalLYg";
@@ -20,6 +21,7 @@ export class Tab2Page {
     // public navCtrl:NavController,
     private dom: DomSanitizer
   ) {
+    this.measureChange = true;
     // setInterval(() => {
     //   this.test++;
     // }, 1000)
@@ -44,9 +46,9 @@ export class Tab2Page {
         ],
         datasets: [
           {
-            label: "Kyle's weight change",
+            label: "Kyle's weight change ( Kg )",
             fill: false,
-            lineTension: 0.1,
+            lineTension: 0.2,
             backgroundColor: "rgba(75,192,192,0.4)",
             borderColor: "rgba(75,192,192,1)",
             borderCapStyle: "butt",
@@ -62,16 +64,32 @@ export class Tab2Page {
             pointHoverBorderWidth: 2,
             pointRadius: 1,
             pointHitRadius: 10,
-            data: [85, 81, 80, 81, 79, 73, 71],
+            data: [85, 81, 80, 81, 79, 73, 71, 81],
+            spanGaps: false
+          },
+          {
+            label: "My weight change",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(115, 18, 18, 0.4)",
+            borderColor: "rgba(115, 18, 18,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(115, 18, 18,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(115, 18, 18,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 5,
+            pointHitRadius: 10,
+            data: [90, 88, 89, 91, 91, 90, 92],
             spanGaps: false
           }
         ]
-      },
-      options: {
-        title: {
-          text: "hello",
-          display: true
-        }
       }
     },
     second: {
@@ -141,13 +159,56 @@ export class Tab2Page {
 
   updateConfigByMutating() {
     // this.lineChart.options.title.text = "new title";
-    // this.lineChart.update();
-    // alert("hello");
-  this.lineChart.data.datasets[0].data[2] = 30;
+    // this.lineChart.data.datasets[0].data = [90, 100, 110, 120, 50, 140, 150, 160];
+
+    this.lineChart.update();
+    alert("hello");
+  }
+
+  updataConfigAboutMonth(month) {
+    
+    let past3month = ["July","August","September","October", "November", "December/18"];
+    for(let i=5;i>=0;i--) {
+      this.lineChart.data.labels.unshift(past3month[i]);
+    }
+    this.lineChart.data.datasets.forEach(dataset => {
+      dataset.data.unshift(80);
+      dataset.data.unshift(83);
+      dataset.data.unshift(81);
+      dataset.data.unshift(89);
+      dataset.data.unshift(87);
+      dataset.data.unshift(86);
+      
+    });
+    this.lineChart.update();
 
   }
 
-  updataConfigAboutMonth(monthValue) {
+  // conversion measure of data
+  dataConverse() {
+    let userData_weight = this.lineChart.data.datasets[0].data;
 
+    if (this.measureChange) {
+      userData_weight.forEach((item, index, array) => {
+        array[index] = item * 2.2;
+        array[index] = array[index].toFixed(2);
+        // function of javascript calculating error fix
+      });
+
+      this.lineChart.data.datasets[0].label = "Kyle's weight change ( LB )";
+      this.lineChart.update();
+      this.measureChange = false;
+    } else {
+      userData_weight.forEach((item, index, array) => {
+        array[index] = item / 2.2;
+        array[index] = array[index].toFixed(2);
+        // function of javascript calculating error fix
+      });
+
+      this.lineChart.data.datasets[0].label = "Kyle's weight change ( KG )";
+      this.lineChart.update();
+      this.measureChange = true;
+    }
+    console.log(userData_weight);
   }
 }
